ea7db7aa81c690d129c98fe6458cc2f2d1d33359
Ergin TIRAVOGLU <ergintiravoglu@msn.com>
2023-09-02 01:56:52 +0300

    Topic Exchange Tamamlandı

--- UdemyRabbitMQExchanges.Publisher/Program.cs
+++ UdemyRabbitMQExchanges.Publisher/Program.cs
@@ -18,18 +18,34 @@ using var connection = factory.CreateConnection();
 
 var channel = connection.CreateModel();
 
-channel.QueueDeclare("hello-queue", true, false, false);
+channel.ExchangeDeclare("logs-topic", type: ExchangeType.Topic, durable: true); //durable:false Uygulama restart atarsa Exchageler silinir
+
+Random rnd = new Random();
 
 Enumerable.Range(1, 50).ToList().ForEach(x =>
-{
-    string message = $"Message {x}";
+{ 
+    LogNames log1 = (LogNames)rnd.Next(1, 5);
+    LogNames log2 = (LogNames)rnd.Next(1, 5);
+    LogNames log3 = (LogNames)rnd.Next(1, 5);
+
+    string message = $"Log Type {log1}.{log2}.{log3} -> Log Message {x}";
 
     var messageBody = Encoding.UTF8.GetBytes(message);
 
-    channel.BasicPublish(string.Empty, "hello-queue", null, messageBody);
+    var routeKey = $"{log1}.{log2}.{log3}";
 
-    Console.WriteLine($"Mesaj Gönderilmiştir : {message}");
+    channel.BasicPublish("logs-topic", routeKey, null, messageBody);
+
+    Console.WriteLine($"Mesaj Log Gönderilmiştir : {message}");
 
 });
 
 Console.ReadLine();
+
+public enum LogNames
+{
+    Critical = 1,
+    Error = 2,
+    Warning = 3,
+    Info = 4
+}
