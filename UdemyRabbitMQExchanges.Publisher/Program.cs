bfdf8a5d7a090f749fb298d84b9995025319881a
Ergin TIRAVOGLU <ergintiravoglu@msn.com>
2023-09-01 22:47:45 +0300

    main Branch init

--- UdemyRabbitMQExchanges.Publisher/Program.cs
+++ UdemyRabbitMQExchanges.Publisher/Program.cs
@@ -1,2 +1,35 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Microsoft.Extensions.Configuration;
+using RabbitMQ.Client;
+using System.Text;
+
+Console.WriteLine("RabbitMQ Publisher");
+
+IConfigurationRoot config = new ConfigurationBuilder().AddJsonFile("appsettings.json").AddEnvironmentVariables().Build();
+var section = config.GetSection("Settings");
+var rabbitMQUrl = section.GetValue<string>("RabbbitMQUrl");
+
+
+var factory = new ConnectionFactory();
+
+factory.Uri = new Uri(rabbitMQUrl);
+
+using var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+
+channel.QueueDeclare("hello-queue", true, false, false);
+
+Enumerable.Range(1, 50).ToList().ForEach(x =>
+{
+    string message = $"Message {x}";
+
+    var messageBody = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish(string.Empty, "hello-queue", null, messageBody);
+
+    Console.WriteLine($"Mesaj Gönderilmiştir : {message}");
+
+});
+
+Console.ReadLine();
