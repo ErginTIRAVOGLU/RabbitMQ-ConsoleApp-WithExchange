e5bc93d20ad4f3645a59420860e3b05c5981e70a
Ergin TIRAVOGLU <ergintiravoglu@msn.com>
2023-09-01 23:41:06 +0300

    Direct-Exchange Tamamlandı

--- UdemyRabbitMQExchanges.Publisher/Program.cs
+++ UdemyRabbitMQExchanges.Publisher/Program.cs
@@ -1,2 +1,61 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Microsoft.Extensions.Configuration;
+using RabbitMQ.Client;
+using System.Text;
+
+Console.WriteLine("RabbitMQ Publisher");
+
+IConfigurationRoot config = new ConfigurationBuilder().AddJsonFile("appsettings.json").AddEnvironmentVariables().Build();
+var section = config.GetSection("Settings");
+var rabbitMQUrl = section.GetValue<string>("RabbbitMQUrl");
+
+
+var factory = new ConnectionFactory();
+
+factory.Uri = new Uri(rabbitMQUrl);
+
+using var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+
+//channel.QueueDeclare("hello-queue", true, false, false); // Kuyruğu Subscriber oluştursun
+
+//channel.ExchangeDeclare("logs-fanout", type: ExchangeType.Fanout, durable: true); //durable:false Uygulama restart atarsa Exchageler silinir
+channel.ExchangeDeclare("logs-direct", type: ExchangeType.Direct, durable: true); //durable:false Uygulama restart atarsa Exchageler silinir
+
+Enum.GetNames(typeof(LogNames)).ToList().ForEach(name =>
+{
+    var routeKey = $"route-{name}";
+    var queueName = $"direct-queue-{name}";
+    channel.QueueDeclare(queueName, true, false, false);
+    channel.QueueBind(queueName, "logs-direct",routeKey,null);
+
+});
+
+Enumerable.Range(1, 50).ToList().ForEach(x =>
+{
+    LogNames logName = (LogNames)new Random().Next(1, 5);
+
+    //string message = $"Log Message {x}";
+    string message = $"Log Type {logName} -> Log Message {x}";
+
+    var messageBody = Encoding.UTF8.GetBytes(message);
+
+    var routeKey=$"route-{logName}";
+
+    //channel.BasicPublish(string.Empty, "hello-queue", null, messageBody);
+    channel.BasicPublish("logs-direct", routeKey, null, messageBody);
+
+    Console.WriteLine($"Mesaj Log Gönderilmiştir : {message}");
+
+});
+
+Console.ReadLine();
+
+public enum LogNames
+{
+    Critical=1,
+    Error=2,
+    Warning=3,
+    Info=4
+}
--- UdemyRabbitMQExchanges.Subscriber/Program.cs
+++ UdemyRabbitMQExchanges.Subscriber/Program.cs
@@ -18,26 +18,43 @@ using var connection = factory.CreateConnection();
 
 var channel = connection.CreateModel();
 
-channel.QueueDeclare("hello-queue", true, false, false);
+//channel.QueueDeclare("hello-queue", true, false, false);
 
-//channel.BasicQos(0, 3, false); // Her bir istemci üç üç alır
-//channel.BasicQos(0, 6, false); // Kaç istemci varsa 6'ya böler (3 istemci varsa 2-2-2 şeklinde alır)
+//channel.ExchangeDeclare("logs-fanout", type: ExchangeType.Fanout, durable: true); //durable:false Uygulama restart atarsa Exchageler silinir
+
+////var randomQueueName = channel.QueueDeclare().QueueName; //
+
+//var randomQueueName = "logs-database-save-queue";
+
+//channel.QueueDeclare(randomQueueName, true, false, false);
+
+//channel.QueueBind(randomQueueName, "logs-fanout", "", null);
+
+
 
 channel.BasicQos(0, 1, false); // Her bir istemci bir bir alır
 
 var consumer = new EventingBasicConsumer(channel);
 
 //channel.BasicConsume("hello-queue", true, consumer); // Alınan mesajı otomatik siler
-channel.BasicConsume("hello-queue", false, consumer); // Mesajı RabbitMQ'den silmek için aşağıdaki komuta ihtiyacı var
+//channel.BasicConsume("hello-queue", false, consumer); // Mesajı RabbitMQ'den silmek için aşağıdaki komuta ihtiyacı var
+
+var queueName = "direct-queue-Critical";
+channel.BasicConsume(queueName, false, consumer);
+
+Console.WriteLine("Loglar Dinleniyor...");
 
 consumer.Received += (object? sender, BasicDeliverEventArgs e) =>
 {
     var message = Encoding.UTF8.GetString(e.Body.ToArray());
     Thread.Sleep(1500);
     Console.WriteLine("Gelen Mesaj : " + message);
+
+    //File.AppendAllText("logs-critical.txt",message + "\n",Encoding.UTF8);
     channel.BasicAck(e.DeliveryTag, false); // Mesajı siler
 };
 
 
 
 Console.ReadLine();
+
