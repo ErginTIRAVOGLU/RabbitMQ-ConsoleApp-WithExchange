bb9d9adf35a9236e4887dc7e01a22e52ffbcfa72
Ergin TIRAVOGLU <ergintiravoglu@msn.com>
2023-09-01 22:00:20 +0300

    Fanout Exchange Tamamlandı.

---
+++ UdemyRabbitMQExchanges.Publisher/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Extensions.Configuration;
+using RabbitMQ.Client;
+using System.Text;
+
+Console.WriteLine("RabbitMQ Publisher");
+
+IConfigurationRoot config = new ConfigurationBuilder().AddJsonFile("appsettings.json").AddEnvironmentVariables().Build();
+var section = config.GetSection("Settings");
+var rabbitMQUrl = section.GetValue<string>("RabbbitMQUrl");
+
+
+var factory = new ConnectionFactory();
+
+factory.Uri = new Uri(rabbitMQUrl);
+
+using var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+
+//channel.QueueDeclare("hello-queue", true, false, false); // Kuyruğu Subscriber oluştursun
+
+channel.ExchangeDeclare("logs-fanout", type:ExchangeType.Fanout, durable:true); //durable:false Uygulama restart atarsa Exchageler silinir
+
+Enumerable.Range(1, 50).ToList().ForEach(x =>
+{
+    string message = $"Log Message {x}";
+
+    var messageBody = Encoding.UTF8.GetBytes(message);
+
+    //channel.BasicPublish(string.Empty, "hello-queue", null, messageBody);
+    channel.BasicPublish("logs-fanout", "", null, messageBody);
+
+    Console.WriteLine($"Mesaj Gönderilmiştir : {message}");
+
+});
+
+Console.ReadLine();
--- UdemyRabbitMQExchanges.Subscriber/Program.cs
+++ UdemyRabbitMQExchanges.Subscriber/Program.cs
@@ -1,2 +1,57 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Microsoft.Extensions.Configuration;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+Console.WriteLine("RabbitMQ Subscriber");
+
+IConfigurationRoot config = new ConfigurationBuilder().AddJsonFile("appsettings.json").AddEnvironmentVariables().Build();
+var section = config.GetSection("Settings");
+var rabbitMQUrl = section.GetValue<string>("RabbbitMQUrl");
+
+var factory = new ConnectionFactory();
+
+factory.Uri = new Uri(rabbitMQUrl);
+
+using var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+
+//channel.QueueDeclare("hello-queue", true, false, false);
+
+channel.ExchangeDeclare("logs-fanout", type: ExchangeType.Fanout, durable: true); //durable:false Uygulama restart atarsa Exchageler silinir
+
+//var randomQueueName = channel.QueueDeclare().QueueName; //
+
+var randomQueueName = "logs-database-save-queue";
+
+channel.QueueDeclare(randomQueueName, true, false,false);
+
+channel.QueueBind(randomQueueName, "logs-fanout", "", null);
+
+
+
+channel.BasicQos(0, 1, false); // Her bir istemci bir bir alır
+
+var consumer = new EventingBasicConsumer(channel);
+
+//channel.BasicConsume("hello-queue", true, consumer); // Alınan mesajı otomatik siler
+//channel.BasicConsume("hello-queue", false, consumer); // Mesajı RabbitMQ'den silmek için aşağıdaki komuta ihtiyacı var
+
+channel.BasicConsume(randomQueueName, false, consumer);
+
+Console.WriteLine("Loglar Dinleniyor...");
+
+consumer.Received += (object? sender, BasicDeliverEventArgs e) =>
+{
+    var message = Encoding.UTF8.GetString(e.Body.ToArray());
+    Thread.Sleep(1500);
+    Console.WriteLine("Gelen Mesaj : " + message);
+    channel.BasicAck(e.DeliveryTag, false); // Mesajı siler
+};
+
+
+
+Console.ReadLine();
+
